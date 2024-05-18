4295a94e9995925fdddb19577bac7f6ced647a1b
oroshi <oroshinekko@gmail.com>
2024-05-18 18:13:53 +0900

    lehmer measure

--- work/lib/arctan.py
+++ work/lib/arctan.py
@@ -1,59 +1,89 @@
-from typing import Generator, Tuple
-from dataclasses import dataclass
+from pydantic import Field, BaseModel
 import mpmath
+from fractions import Fraction
+import math
 
-@dataclass
-class ArctanParam:
+
+def sign(x):
+    return int(math.copysign(1, x))
+
+
+class ArctanParam(BaseModel):
     coeff: int
-    numer: int  # 分子
-    denom: int  # 分母
-    
+    numer: int = Field(..., ge=1)  # 分子
+    denom: int = Field(..., ge=1)  # 分母
+
     @property
     def x(self) -> float:
         return mpmath.mpf(self.numer) / self.denom
+    
+    def composit(self) -> "ArctanParam":
+        x = Fraction(self.numer, self.denom)
+        ret = x
         
-    def __post_init__(self):
-        if self.coeff <= 0:
-            raise ValueError(f"coeff must be greater than 0. given {self.coeff}")
-        if self.numer <= 0:
-            raise ValueError(f"numer must be greater than 0. given {self.numer}")
-        if self.denom <= 0:
-            raise ValueError(f"denom must be greater than 0. given {self.denom}")
+        for _ in range(1, abs(self.coeff)):
+            ret = (ret + x) / (1 - ret * x)
+        
+        return ArctanParam(
+            coeff = sign(self.coeff),
+            numer = ret.numerator,
+            denom = ret.denominator
+        )
+    
+    def add(self, atan_param: "ArctanParam") -> "ArctanParam":
+        tmp = self.composit()
+        a = Fraction(tmp.coeff * tmp.numer, tmp.denom)
+        
+        tmp = atan_param.composit()
+        b = Fraction(tmp.coeff * tmp.numer, tmp.denom)
+        
+        ret =  (a + b) / (1 - a * b)
+        
+        return ArctanParam(
+            coeff = sign(ret.numerator),
+            numer = abs(ret.numerator),
+            denom = ret.denominator
+        )
+    
+    def equal_to(self, atan_param: "ArctanParam") -> bool:
+        if self.coeff == atan_param.coeff \
+                and self.numer == atan_param.numer \
+                and self.denom == atan_param.denom:
+            return True
+        else:
+            return False
+    
 
-            
 class Series():
     _n: int
     _term: float
     _val: float  # sum
     x: float
-        
+
     @property
     def n(self) -> int:
         return self.n
-    
+
     @property
     def term(self) -> float:
         return self._term
-    
+
     @property
     def val(self) -> float:
         return self._val
-    
+
     def _next_term(self) -> float:
         raise NotImplementedError()
-        
-    def __next__(self) -> float:
-        raise NotImplementedError()
-        
+
     def __next__(self) -> float:
         self._term = self._next_term()
-        self._sum = self._sum + self._term
+        self._val = self._val + self._term
         self._n = self._n + 1
-        return self._sum
-        
+        return self._val
+
 
 class ArctanSeries(Series):
-    
+
     def __init__(self, p: ArctanParam):
         self._n = 0
         self.param = p
@@ -65,12 +95,21 @@ class ArctanSeries(Series):
         # a_{n+1} を返す
         return (-1) * (mpmath.mpf(2 * self._n + 1) / (2 * self._n + 3)) \
                 * self._term * (self.x ** 2)
-        
-        
-def EulerTransformSeries(p: ArctanParam):
+
+
+class EulerTransformSeries(Series):
     def __init__(self, p: ArctanParam):
         self._n = 0
         self.param = p
-        x = self.param.x
-        
-        
+        self.x = self.param.x
+
+        self._term = mpmath.mpf(p.coeff) * (self.x / (1 + self.x ** 2))
+        self._val = self._term
+
+    def _next_term(self) -> float:
+        # a_{n+1} を返す
+        return (
+            mpmath.mpf(2 * self.n + 2) / (2 * self.n + 3)
+        ) * self._val * (
+            self.x ** 2 / (1 + self.x ** 2)
+        )
